2869b58bdea2f9fbb0f167572bbcec61b3a6917f
Jacob Shusko <jws383@cornell.edu>
2020-04-25 16:31:52 -0400

    finished python scripts (not currently useful without a change in the source code of pytrend)

--- trendsPull.py
+++ trendsPull.py
@@ -1,29 +1,48 @@
 ### Author: Jacob Shusko (jws383@cornell.edu) ###
-### Date: April 22, 2020 ### 
+### Date: April 25, 2020 ### 
 ### Refs: https://towardsdatascience.com/google-trends-api-for-python-a84bc25db88f
 
-## This script pulls google trends data for US-NY and US-CA metropolitan areas 
-## related to Covid-19 symptoms and updates the "covid19_googleTrends.csv" in 
-## the following repo: https://github.com/jshusko/covid19_googleTrends.git
+"""
+This script pulls google trends data for x countries related to Covid-19 symptoms
+and updates the "covid19_googleTrends.csv" in the following repository:
+		https://github.com/jshusko/covid19_googleTrends.git
+"""
 
-import pandas as pd
-import matplotlib
-from pytrends.request import TrendReq
+import pandas as pd 
+from pytrends.request import TrendReq #https://github.com/GeneralMills/pytrends
 
 
-pytrend = TrendReq(hl='en-US',tz=360,retries=10,backoff_factor=0.5)
+pytrend = TrendReq(hl='en-US',tz=360,retries=20,backoff_factor=0.5)
 
-symptoms = ['smell', 'taste']
-pytrend.build_payload(kw_list=symptoms)
+# have to perform pull in batches due to google trends limit of 5 terms
+symptoms = ['smell', 'taste', 'sore', 'throat', 'upset stomach', 'diarrhea', 
+       'no appetite', 'difficulty breathing', 'fever', 'sore eyes', 
+       'runny nose', 'fatigue', 'dry cough', 'chest pain', 'blue face']
+symptoms_1 = symptoms[0:5]
+symptoms_2 = symptoms[5:10]
+symptoms_3 = symptoms[10:len(symptoms)]
 
-# interest by region
-df = pytrend.interest_by_region(resolution='DMA',inc_low_vol=True,inc_geo_code=True)
-print(df.head(10))
+# batch 1 
+pytrend.build_payload(kw_list=symptoms_1)
+df1 = pytrend.get_historical_interest(keywords=symptoms_1,year_start=2019,month_start=12,hour_start=0,day_start=1,year_end=2020,month_end=3,hour_end=23,day_end=31)
+print(df1.head(20))
 
-#parse 
-NY_counties = ["Binghamton NY", 'Watertown NY', 'Buffalo NY', 'Utica NY', 
-               'Syracuse NY', 'Rochester NY', 'Albany-Schenectady-Troy NY'
-               'Elmira NY', 'New York NY', 'Burlington VT-Plattsburgh NY']
+# # batch 2 
+# pytrend.build_payload(kw_list=symptoms_2)
+# df2 = pytrend.interest_by_region(resolution='DMA',inc_low_vol=True,inc_geo_code=True)
+# print(df2.head(10))
+
+# # batch 3
+# pytrend.build_payload(kw_list=symptoms_3)
+# df3 = pytrend.interest_by_region(resolution='DMA',inc_low_vol=True,inc_geo_code=True)
+# print(df3.head(10))
+
+# # combine batches
+# result = (pd.concat([df1,df2,df3], axis=1, sort=False)).drop(['geoCode'],axis=1)
+# print(result.head(10))
+
+# # select NY county data
+# print(result.index)
 
 # read out to csv
-df.to_csv('covid19_googleTrends.csv',encoding='utf-8')
+# result_NY.to_csv('covid19_googleTrends.csv',encoding='utf-8')
---
+++ trendsPull_county.py
@@ -0,0 +1,52 @@
+### Author: Jacob Shusko (jws383@cornell.edu) ###
+### Date: April 22, 2020 ### 
+### Refs: https://towardsdatascience.com/google-trends-api-for-python-a84bc25db88f
+
+## This script pulls google trends data for US-NY and US-CA metropolitan areas 
+## related to Covid-19 symptoms and updates the "covid19_googleTrends.csv" in 
+## the following repo: https://github.com/jshusko/covid19_googleTrends.git
+
+import pandas as pd 
+import matplotlib
+from pytrends.request import TrendReq
+
+
+pytrend = TrendReq(hl='en-US',tz=360,retries=20,backoff_factor=0.5)
+
+# have to perform pull in batches due to google trends limit of 5 terms
+symptoms = ['smell', 'taste', 'sore', 'throat', 'upset stomach', 'diarrhea', 
+       'no appetite', 'difficulty breathing', 'fever', 'sore eyes', 
+       'runny nose', 'fatigue', 'dry cough', 'chest pain', 'blue face']
+symptoms_1 = symptoms[0:5]
+symptoms_2 = symptoms[5:10]
+symptoms_3 = symptoms[10:len(symptoms)]
+
+# batch 1 
+pytrend.build_payload(kw_list=symptoms_1)
+df1 = pytrend.interest_by_region(resolution='DMA',inc_low_vol=True,inc_geo_code=True)
+df1_a = pytrend.interest_over_time()
+print(df1_a.head(20))
+
+# batch 2 
+pytrend.build_payload(kw_list=symptoms_2)
+df2 = pytrend.interest_by_region(resolution='DMA',inc_low_vol=True,inc_geo_code=True)
+print(df2.head(10))
+
+# batch 3
+pytrend.build_payload(kw_list=symptoms_3)
+df3 = pytrend.interest_by_region(resolution='DMA',inc_low_vol=True,inc_geo_code=True)
+print(df3.head(10))
+
+# combine batches
+result = (pd.concat([df1,df2,df3], axis=1, sort=False)).drop(['geoCode'],axis=1)
+print(result.head(10))
+
+# select NY county data
+NY_counties = ["Binghamton NY", 'Watertown NY', 'Buffalo NY', 'Utica NY', 
+               'Syracuse NY', 'Rochester NY', 'Albany-Schenectady-Troy NY',
+               'Elmira NY', 'New York NY', 'Burlington VT-Plattsburgh NY']
+result_NY = result[(result.index).isin(NY_counties)]
+print(result.index)
+
+# read out to csv
+result_NY.to_csv('covid19_googleTrends.csv',encoding='utf-8')
