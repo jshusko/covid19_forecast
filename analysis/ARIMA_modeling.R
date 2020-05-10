22197db56715cdb41198ccf36a8ad206405b26de
Jacob Shusko <jws383@cornell.edu>
2020-05-10 14:54:33 -0400

    need to figure out how to append models

--- analysis/ARIMA_modeling.R
+++ analysis/ARIMA_modeling.R
@@ -1,28 +1,40 @@
 library(dplyr)
 library(ggplot2)
 library(forecast)
+library(anytime)
+library(ggpubr)
 
 ### PREPROCESS ### 
 df_full <- read.csv("./preprocess/master_5-8-20.csv") %>% as_tibble() 
 df_full
 
-## New York City subset 
-la_county_fips = 6037
-ny_county_fips = 36061
-cook_county_fips = 17031
-atl_county_fips = 13135
+### Individual County Simple Model ###
+la = 6037
+ny = 36061
+cook = 17031
+gwinnett = 13135
+harris = 48201
+tompkins = 36109
+suffolk = 25025
+hennepin = 27053 
+hillsborough = 12057
+counties = c(la,ny,cook,gwinnett,harris,tompkins,suffolk,hennepin,hillsborough)
 
-df_deaths <- select(df_full,FIPS,DATE,CUMULATIVE.DEATHS) %>% filter(FIPS==atl_county_fips) %>% as_tibble()
-df_deaths$DATE <- as.Date(df_deaths$DATE, format = "%Y-%m-%d")
-dates_ts <- as.ts(df_deaths$DATE)
-ggplot(data=df_deaths, aes(x=DATE,y=CUMULATIVE.DEATHS),group=1)+geom_line(color='blue',size=2)
-
-forecast_interval = 25
-ny_ts_train <- ts(data=df_deaths$CUMULATIVE.DEATHS, start=min(dates_ts), end=max(dates_ts)-forecast_interval)
-ny_ts <- ts(data=df_deaths$CUMULATIVE.DEATHS, start=min(dates_ts), end=max(dates_ts))
-
-fit <- auto.arima(ny_ts_train)
-forecast <- forecast(fit,h=forecast_interval)
-autoplot(forecast)+autolayer(ny_ts)
-
-
+forecast_interval = 21
+plots = c()
+fits = c()
+j=1
+for (i in counties) {
+  print(j)
+  df_deaths <- select(df_full,FIPS,DATE,CUMULATIVE.DEATHS) %>% filter(FIPS==i) %>% as_tibble()
+  df_deaths$DATE <- as.Date(df_deaths$DATE, format = "%Y-%m-%d")
+  dates_ts <- as.ts(df_deaths$DATE)
+  ts_train <- ts(data=df_deaths$CUMULATIVE.DEATHS, start=min(dates_ts), end=max(dates_ts)-forecast_interval)
+  ts_actual <- ts(data=df_deaths$CUMULATIVE.DEATHS, start=min(dates_ts), end=max(dates_ts))
+  fit <- auto.arima(ny_ts_train,stepwise = FALSE, approximation = FALSE)
+  forecast <- forecast(fit,h=forecast_interval)
+  plot <- autoplot(forecast) + autolayer(ny_ts)
+  append(fits,fit)
+  append(plots,plot)
+  j <- j+1
+}
